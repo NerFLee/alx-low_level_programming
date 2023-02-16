6577b62e4636f38354b701d2ba877d8651d43879
NerFLee <schidmike70@gmail.com>
2023-02-16 15:00:45 -0800

    christ

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Write a C program that executes the fuction 'printf' on a new line.
+ * main -return an exit status to the operating system. Write a C program that executes the fuction 'printf' on a new line.
  *
  * return: 0
 */
-int main(void)
+ int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
