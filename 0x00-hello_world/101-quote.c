4e06f15402220e7543696cebf76467cb2f01c864
NerFLee <schidmike70@gmail.com>
2023-02-16 17:31:09 -0800

    lord

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main -a c program that prints a line to the standard error
- * return 1 (Success)
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
